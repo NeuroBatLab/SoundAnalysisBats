436370d5d77300a1101a33f41198b5cfde6e6f50
Julie Elie <julie.elie@gmail.com>
2020-02-14 09:13:54 -0800

    saving data within each intput file

--- cal_duration_acoustic_state.m
+++ cal_duration_acoustic_state.m
@@ -1,13 +1,15 @@
 
-function [DAS]=cal_duration_acoustic_state(BioSoundPath)
+function cal_duration_acoustic_state(BioSoundPath)
 
-% 1. Calculate acoustic features in time i (one FFT frame): pitch, FM, Wiener entropy, Goodness of pitch
-% 2. Calculate same features in itime i+1
-% 3. scale the features according to the table below (subtract median and divide by MAD)
-% 4. calculate Euclidean distance across features, e.i., sqrt (pitch i - pitch i+1)^2 + sqrt(FM....
-% 5. if distance < 2 (MADs) repeat for i+2, i+3... each time using the same origin , i, as reference, until distance from origin is =>2
-% 6. Repeat for i-1, ... i-2... 
-% 7 DAS is the interval around i, where MAD<2
+% 1. Import from Biosound time varying acoustic features: pitch saliency,
+% spectral mean and amplitude
+% 2. calculate the median and the MAD of each features accross all times
+% points and all files
+% 3. scale the time varying features by subtracting median and dividing by MAD
+% 4. at each time point i calculate Euclidean distance between i and i+1 across features, e.i., sqrt (Sal i - Sal i+1)^2 + sqrt(SM....
+% if distance < 2 (MADs) repeat for i+2, i+3... each time using the same origin , i, as reference, until distance from origin is =>2
+% Repeat for i-1, ... i-2... 
+% 7. DAS is the interval around i, where euclidian distance in unit of MAD<2
 
 %% List the data
 AllBioS = dir(fullfile(BioSoundPath, '*.mat'));
@@ -51,8 +53,6 @@ MADSpecMean = median(abs(AllSpecMean - MedianSpecMean),'omitnan');
 
 %% Loop again through files correct input values and calculate DAS
 for ff=1:NFiles
-    % Initialize the output variable
-    DAS = cell(NFiles,1);
     % load the data for that bat/file
     load(fullfile(AllBioS(ff).folder,AllBioS(ff).name),'BioSoundCall')
     % correct values by median and MAD
@@ -63,7 +63,7 @@ for ff=1:NFiles
     SpecMean = (BioSoundCall.SpectralMean(1:end-1) - MedianSpecMean)/MADSpecMean;
     % Now iterate through time point and calculate DAS
     NP = length(Sal);
-    DAS{ff} = nan(NP,1);
+    DAS = nan(NP,1);
     for pp=1:NP
         % iterate through future time point and find when euclidian
         % distance >2
@@ -82,7 +82,8 @@ for ff=1:NFiles
                 break
             end
         end
-        DAS{ff}(pp) = ipast + ifuture -2;
+        DAS(pp) = ipast + ifuture -2;
     end
+    save(fullfile(AllBioS(ff).folder,AllBioS(ff).name),'DAS','-append')
 end
 
