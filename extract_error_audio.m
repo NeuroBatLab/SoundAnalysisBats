51bda91f506e31faf5d634b47eb2f307f7bdaae8
nwang19 <wangnicole19@gmail.com>
2020-04-25 17:44:22 -0700

    Update extract_error_audio.m
    Added dates to iterate through, fixed onset/offset

--- extract_error_audio.m
+++ extract_error_audio.m
@@ -1,38 +1,53 @@
-function extract_error_audio(Loggers_dir, Date, ExpStartTime)
 % Converts vocal extraction data in .mat files into relevant .wav files,
 % separating the audio by type. Writes into folder Z:\tobias\vocOperant\error_clips
 
 OutputDataPath = 'Z:\tobias\vocOperant\error_clips';
-DataFile = fullfile(Loggers_dir, sprintf('%s_%s_VocExtractData.mat', Date, ExpStartTime));
-load(DataFile, 'Piezo_wave', 'Piezo_FS',  'Raw_wave','FS', 'RatioRMS', 'DiffRMS','BandPassFilter', 'AudioLogs', 'RMSHigh', 'RMSLow','VocFilename');
+BoxOfInterest = [3 4 6 8];
 
-Nvoc = length(Raw_wave);
-IndVocStart_all = cell(1,Nvoc);
-IndVocStop_all = cell(1,Nvoc);
-IndNoiseStart_all = cell(1,Nvoc);
-IndNoiseStop_all = cell(1,Nvoc);
-IndVocStartRaw = cell(1,Nvoc);% Contains for each sequence of vocalizations (Nvoc)
-% a cell array of the size the number of loggers + 1 in case only one bat without a logger
-% or +2 incase no identification possible but you want to keep onset/offset of each voc and
-% for each logger the index onset of when the animal start vocalizing in the raw recording before merge
-IndVocStartPiezo = cell(1,Nvoc);% Contains for each sequence of vocalizations (Nvoc) a cell array of the size the number of loggers and for each logger the index onset of when the animal start vocalizing in the piezo recording before merge
-IndVocStopRaw = cell(1,Nvoc);% Contains for each sequence of vocalizations (Nvoc) a cell array of the size the number of loggers and for each logger the index offset of when the animal stop vocalizingin the raw recording before merge
-IndVocStopPiezo = cell(1,Nvoc);% Contains for each sequence of vocalizations (Nvoc) a cell array of the size the number of loggers and for each logger the index offset of when the animal stop vocalizingin the piezo recording before merge
-IndNoiseStartRaw = cell(1,Nvoc);% Contains for each sequence of vocalizations (Nvoc) a cell array of the size the number of loggers and for each logger the index onset of when the animal start vocalizing in the raw recording before merge
-IndNoiseStartPiezo = cell(1,Nvoc);% Contains for each sequence of vocalizations (Nvoc) a cell array of the size the number of loggers and for each logger the index onset of when the animal start vocalizing in the piezo recording before merge
-IndNoiseStopRaw = cell(1,Nvoc);% Contains for each sequence of vocalizations (Nvoc) a cell array of the size the number of loggers and for each logger the index offset of when the animal stop vocalizingin the raw recording before merge
-IndNoiseStopPiezo = cell(1,Nvoc);% Contains for each sequence of vocalizations (Nvoc) a cell array of the size the number of loggers and for each logger the index offset of when the animal stop vocalizingin the piezo recording before merge
-IndVocStartRaw_merged = cell(1,Nvoc);% Contains for each sequence of vocalizations (Nvoc) a cell array of the size the number of loggers and for each logger the index onset of when the animal start vocalizing in the raw recording
-IndVocStopRaw_merged = cell(1,Nvoc);% Contains for each sequence of vocalizations (Nvoc) a cell array of the size the number of loggers and for each logger the index offset of when the animal stop vocalizingin the raw recording
-IndVocStartPiezo_merged = cell(1,Nvoc);% Contains for each sequence of vocalizations (Nvoc) a cell array of the size the number of loggers and for each logger the index onset of when the animal start vocalizing in the piezo recording
-IndVocStopPiezo_merged = cell(1,Nvoc);% Contains for each sequence of vocalizations (Nvoc) a cell array of the size the number of loggers and for each logger the index offset of when the animal stop vocalizingin the piezo recording
+for bb=1:length(BoxOfInterest)
+    DatesDir = dir(fullfile('Z:\tobias\vocOperant\box3\piezo',sprintf('box%d',BoxOfInterest(bb)),'piezo', '1*'));
+    for dd=1:length(DatesDir)
+        indsrc = dir(fullfile(DatesDir(dd).folder, DatesDir(dd).name,'audiologgers', '*_VocExtractData_*'));
+        wavsrc = dir(fullfile(DatesDir(dd).folder, DatesDir(dd).name,'audiologgers', '*_VocExtractData'));
+        for ff=1:length(wavsrc)
+            if (~isempty(wavsrc)) && (~isempty(indsrc))
+                load(fullfile(wavsrc(ff).folder,wavsrc(ff).name), 'Raw_wave','FS');
+                load(fullfile(indsrc(ff).folder,indsrc(ff).name),  'IndVocStartRaw', 'IndVocStopRaw', 'IndNoiseStartRaw', 'IndNoiseStopRaw');
 
-% convert with / 1000 * FS
-clips = cell(Nvoc);
-clip_names = cell(Nvoc);
-for vv=vv:Nvoc
-  clips(vv) = Raw_wave(vv, IndVocStart_all / 1000 * FS, IndVocStop_all / 1000 * FS);
-  clip_names(vv) = VocFilename(vv);
+                % Filter for the Mic signal
+                [z,p,k] = butter(3,100/(FS/2),'high');
+                sos_high_raw = zp2sos(z,p,k);
 
-  audiowrite(append(OutputDataPath, clip_names(vv)), clips(vv), FS)
+                % Do we even need to save the clip names? Use this if yes:
+                % clip_names = cell(sum(cellfun(@length,IndVocStartRaw)));
+                % clip_names(vv * (length(IndVocStartRaw(vv)) - 1) + logger) = ...
+
+                Nvoc = length(Raw_wave);
+                for vv=1:Nvoc
+                  WL = Raw_wave(vv);
+                    for logger=1:length(IndVocStartRaw(vv))
+                        %[WL, FS] = audioread(fullfile(WaveFiles(vv).folder, WaveFiles(vv).name));
+                        % convert with / 1000 * FS ??
+
+                        indices = IndVocStartRaw(vv);
+                        vocSnippet = WL(indices(logger):indices(logger));
+                        % filter and center the data
+                        vocFiltWL = filtfilt(sos_high_raw, 1, vocSnippet);
+                        vocFiltWL = vocFiltWL-mean(vocFiltWL);
+                        % get name and write to file
+                        voc_name = sprintf('%s_%d.wav', wavsrc(ff).name(1:end-4), indices(logger));
+                        audiowrite(fullfile(OutputDataPath, voc_name), vocSnippet, FS)
+
+                        %repeat lines 48-54 for IndNoiseStart/Stop
+                        indices = IndNoiseStartRaw(vv);
+                        noiseSnippet = WL(indices(logger):indices(logger));
+                        noiseFiltWL = filtfilt(sos_high_raw, 1, noiseSnippet);
+                        noiseFiltWL = noiseFiltWL-mean(noiseFiltWL);
+                        noise_name = sprintf('%s_%d.wav', wavsrc(ff).name(1:end-4), indices(logger));
+                        audiowrite(fullfile(OutputDataPath, noise_name), noiseSnippet, FS)
+                    end
+                end
+            end
+        end
+    end
 end
