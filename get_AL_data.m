88fc9199fec57fe339dc8a72c13f2d7955492c6f
Maimon <maimon.rose@gmail.com>
2020-07-03 11:13:29 -0700

    updates for adult_social experiments

--- get_AL_data.m
+++ get_AL_data.m
@@ -5,18 +5,25 @@ pnames = {'sessionType', 'boxNum'};
 dflts  = {'communication', NaN};
 [sessionType,boxNum] = internal.stats.parseArgs(pnames,dflts,varargin{:});
 
-addpath('C:\Users\phyllo\Documents\GitHub\SoundAnalysisBats\')
+% addpath('C:\Users\phyllo\Documents\GitHub\SoundAnalysisBats\')
+[~,net_use_str]= dos('net use');
+net_use_str = strsplit(net_use_str,'\n');
+
+net_use_str = net_use_str{contains(net_use_str,'yartsev_server3')};
+remote_drive_letter = regexp(net_use_str,'[A-Z]{1}\:','match');
+remote_drive_letter = remote_drive_letter{1};
+remote_base_dir = fullfile(remote_drive_letter,'users\maimon');
 
 switch expType
     case 'acousticRecording'
         dataDir = 'C:\Users\phyllo\Documents\Maimon\acoustic_recording\';
-        logger_base_dir = 'Z:\users\Maimon\acoustic_recording\video_and_AL\';
-        audio_base_dir = 'Z:\users\Maimon\acoustic_recording\audio\';
+        logger_base_dir = fullfile(remote_base_dir,'\acoustic_recording\video_and_AL\');
+        audio_base_dir = fullfile(remote_base_dir,'acoustic_recording\audio\');
         audio_dir_str = 'audio\ch1\';
         T = readtable([dataDir 'recording_logs.csv']);
         T = T(T.Date==expDate,:);
     case 'adultRecording'
-        dataDir = 'Y:\users\maimon\adult_recording\';
+        dataDir = fullfile(remote_base_dir,'adult_recording');
         logger_base_dir = dataDir;
         audio_base_dir = dataDir;
         audio_dir_str = 'audio\communication\ch1\';
@@ -24,15 +31,15 @@ switch expType
         dateIdx = T.Date==expDate & strcmp(T.Session,sessionType);
         T = T(dateIdx,:);
     case 'adult_social'
-        dataDir = 'Y:\users\maimon\adult_social_recording\';
+        dataDir = fullfile(remote_base_dir,'adult_social_recording');
         logger_base_dir = dataDir;
         audio_base_dir = dataDir;
         audio_dir_str = ['audio\' sessionType '\ch1\'];
-        T = readtable(fullfile(dataDir,'documents\recording_logs.csv'));
+        T = get_rec_logs;
         dateIdx = T.Date==expDate & strcmp(T.Session,sessionType);
         T = T(dateIdx,:);
     case 'operant_adult_recording'
-        dataDir = 'Y:\users\maimon\adult_operant_recording\';
+        dataDir = fullfile(remote_base_dir,'adult_operant_recording');
         logger_base_dir = dataDir;
         audio_base_dir = dataDir;
         T = readtable([dataDir 'documents\recording_logs.csv']);
@@ -69,9 +76,9 @@ logger_nums = T{1,AL_idx};
 logger_nums = logger_nums(~isnan(logger_nums));
 % logger_nums = setdiff(logger_nums,cast(T.malfunction_loggers{:},'double'));
 
-logger_dir = [logger_base_dir dateStr '\audiologgers\'];
+logger_dir = fullfile(logger_base_dir,dateStr,'audiologgers');
 for logger_k = 1:length(logger_nums)
-    data_fname = dir([logger_dir 'logger' num2str(logger_nums(logger_k)) filesep 'extracted_data' filesep '*CSC0.mat']);
+    data_fname = dir(fullfile(logger_dir, ['logger' num2str(logger_nums(logger_k))],'extracted_data','*CSC0.mat'));
     tsData(logger_k) = load(fullfile(data_fname.folder,data_fname.name));
 end
 
